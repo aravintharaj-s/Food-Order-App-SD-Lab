584b5fe9d80330a8dccff48a99cad7ed52c8267f
aravintharaj-s <aravioct@gmail.com>
2021-11-08 12:58:01 +0530

    Signup and login UI fixed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -2,163 +2,129 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sdlad_fos/Pages/menupage.dart';
 import 'package:sdlad_fos/Pages/signup_page.dart';
+import 'package:sdlad_fos/utilities/roundedbutton.dart';
+import 'package:sdlad_fos/utilities/roundedinput_field.dart';
+import 'package:sdlad_fos/utilities/roundedpasswordfield.dart';
 import 'signup_page.dart';
+import 'forgotPassword_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../constants.dart';
-
+import 'package:sdlad_fos/fade_animation.dart';
 
 // ignore: must_be_immutable
 class LoginPage extends StatefulWidget {
   static String id = 'Login_page';
   const LoginPage({Key? key}) : super(key: key);
- @override
+  @override
   _LoginPageState createState() => _LoginPageState();
-  
 }
 
 class _LoginPageState extends State<LoginPage> {
   late String _email, _password;
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.center,
           children: <Widget>[
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.only(top: 20),
-                height: 180,
-                width: 180,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("images/ssn2.jpg"),
-                    alignment: Alignment.bottomCenter,
-                  ),
+            Positioned(
+              top: 0,
+              child: FadeAnimation(
+                2,
+                Container(
+                  child: Image.asset('images/undraw_breakfast.png'),
                 ),
               ),
+              width: size.width,
+              height: size.height * 0.25,
             ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text("SIGN IN",
-                            style: TextStyle(
-                              color: kDarkssn,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 40,
-                            )),
-                        FlatButton(
-                          color: Colors.black12,
-                          textColor: Colors.white,
-                          child: Text('Sign Up',
-                              style: TextStyle(
-                                color: kDarkssn,
-                                fontWeight: FontWeight.bold,
-                              )),
-                          onPressed: ()
-                          {
-                            print('Pressed SignUP!');
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) {
-                                return Signup();
-                              }),
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 40),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(right: 16),
-                            child: Icon(
-                              Icons.alternate_email,
-                              color: kDarkssn,
-                            ),
-                          ),
-                          Expanded(
-                            child: TextField(
-                              onChanged: (value){
-                                _email=value;
-                              },
-                              decoration: InputDecoration(
-                                hintText: "Email Address",
-                              ),
-                            ),
-                          )
-                        ],
+            Positioned(
+              top: 130,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: size.height * 0.03),
+                  RoundedInputField(
+                    c1: kInactiveColor,
+                    h1: 50,
+                    icon: Icons.person,
+                    hintText: "Your Email",
+                    onChanged: (value) {
+                      _email = value;
+                    },
+                  ),
+                  RoundedPasswordField(
+                    c1: kInactiveColor,
+                    h1: 50,
+                    hintText: "Password",
+                    onChanged: (value) {
+                      _password = value;
+                    },
+                  ),
+                  RoundedButton(
+                    text: "LOGIN",
+                    press: () async {
+                      UserCredential user = await FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                              email: _email, password: _password);
+                      if (user != null) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) {
+                            return Login_Successfull();
+                          }),
+                        );
+                      } else {
+                        print('user does not exist');
+                      }
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account ?",
+                        style: TextStyle(color: kDarkssn),
                       ),
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(right: 16),
-                          child: Icon(
-                            Icons.lock,
-                            color: kDarkssn,
-                          ),
-                        ),
-                        Expanded(
-                          child: TextField(
-                            obscureText: true,
-                            onChanged: (value){
-                              _password=value;
-                            },
-                            decoration: InputDecoration(
-                              hintText: "Password",
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Spacer(),
-                    GestureDetector(
-                      child: Container(
-                        color: kDarkssn,
-                        margin: EdgeInsets.only(top: 10.0),
-                        width: double.infinity,
-                        height: 80.0,
-                        child: Center(
-                          child: Text('LogIn',
-                              style: TextStyle(
-                                color: kInactiveColor,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                              )),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, Signup.id);
+                        },
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              color: kDarkssn, fontWeight: FontWeight.bold),
                         ),
                       ),
-                      onTap: ()async
-                      {
-                        UserCredential user= await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
-                        if(user!=null)
-                          {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) {
-                                return Login_Successfull();
-                              }),
-                            );
-                          }
-                        else
-                          {
-                            print('user does not exist');
-                          }
-                      },
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, ForgotPassword.id);
+                    },
+                    child: Text(
+                      "Forgot Password",
+                      style: TextStyle(
+                          color: kDarkssn,
+                          decoration: TextDecoration.underline),
                     ),
-                  ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: FadeAnimation(
+                2,
+                Container(
+                  child: Image.asset('images/login.png'),
+                  width: size.width,
+                  height: size.height * 0.365,
                 ),
               ),
             ),
@@ -168,10 +134,10 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+
 class Login_Successfull extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MenuPage();
-    
   }
 }
